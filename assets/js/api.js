7d1dce690004355290ddef424df3ecf5b83169bc
Lucas Simon <82914132+lucasimonmendes@users.noreply.github.com>
2023-03-17 00:22:06 -0300

    update api url

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = '/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/lucasimonmendes/portfoliojavascript/main/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }

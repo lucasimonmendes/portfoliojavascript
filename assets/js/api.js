113cf95fe5e780bffc76846f644035f5c6cbd044
Lucas Simon <lucasimonmendes@hotmail.com>
2023-03-17 00:13:56 -0300

    resolvendo bug do github pages 2

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = '../data/profile.json'
+    const url = 'data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
